c6c845cefa74fcfa22b5ecbf8ddae55899da8d4f
Panagiotis Christofilopoulos <xristopana@gmail.com>
2026-07-06 15:45:06 +0300

    HM_opt

--- optimization/markowitz.py
+++ optimization/markowitz.py
@@ -3,3 +3,5 @@ import pandas as pd
 
 from scipy.optimize import minimize
 from risk.metrics import compute_portfolio_volatility, compute_portfolio_return
+
+def minimize_volatility_for_target_return(mean_returns, cov_matrix, target_return):
